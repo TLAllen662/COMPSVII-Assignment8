ca36b6b20293c7b6261f5ba6e0381965eb27f516
TLAllen662 <tlallen1419@gmail.com>
2025-10-25 19:00:24 -0500

    Add event listener for answer buttons in script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 console.log("script.js connected!");
+let buttons = document.querySelectAll('.answer-btn');
+    buttons.forEach(function(button) {
+    button.addEventListener('click', function() {
+let answer = this.getAttribute('data-answer');
+    output.textContent = "You selected: " + answer;
+ })
+});
