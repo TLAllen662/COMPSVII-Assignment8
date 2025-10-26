b6ab7e357af4462d00ed66b3120dc86812b51976
TLAllen662 <tlallen1419@gmail.com>
2025-10-25 19:38:23 -0500

    Update answer buttons to include mood data attributes and enhance output logic

--- js/script.js
+++ js/script.js
@@ -2,8 +2,21 @@ console.log("script.js connected!");
 let buttons = document.querySelectAll('.answer-btn');
     buttons.forEach(function(button) {
     button.addEventListener('click', function() {
+    let mood = button.getAttribute('data-mood');
     let output = document.getElementById('output');
     let answer = this.getAttribute('data-answer');
-    output.textContent = "You selected: " + answer;
- })
+    output.textContent = 'You selected:' + answer;
+
+    if (mood === '1') {
+        output.textContent = 'Football Fan!';
+    } else if (mood === '2') {
+        output.textContent = 'Soccer Fan!';
+    } else if (mood === '3') {
+        output.textContent = 'Basketball Fan!';
+    } else if (mood === '4') {
+        output.textContent = 'Baseball Fan!';
+    }
+    
+})
 });
+
