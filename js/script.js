fb34862a5b373f4bbb8bc2e5b3ea6f6dca5c38e6
TLAllen662 <tlallen1419@gmail.com>
2025-10-26 16:47:35 -0500

    Fix typo in button event listener and ensure proper class assignment

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 console.log("script.js connected!");
 let buttons = document.querySelectAll('.answer-btn');
     buttons.forEach(function(button) {
-    button.addEventListener('click', function() {
-    btn.classlist.add('clicked');
-    let mood = button.getAttribute('data-mood');
-    let buttonID = button.dataset.buttonid
-    let response = button.dataset.mood;
-    let output = document.getElementById('output');
-    let answer = this.getAttribute('data-answer');
-    output.textContent = 'You selected:' + answer;
+        button.addEventListener('click', function() {
+            console.log('Button clicked!');
+        btn.classlist.add('clicked');
+        let mood = button.getAttribute('data-mood');
+        let buttonID = button.dataset.buttonid
+        let response = button.dataset.mood;
+        let output = document.getElementById('output');
+        let answer = this.getAttribute('data-answer');
+        output.textContent = 'You selected:' + answer;
 
 
     if (mood === '1') {
