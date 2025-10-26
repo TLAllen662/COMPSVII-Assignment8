52bc5158339dfde8a7f90f9a2836805c078cd779
TLAllen662 <tlallen1419@gmail.com>
2025-10-25 19:03:45 -0500

    Fix output element retrieval in answer button event listener

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ console.log("script.js connected!");
 let buttons = document.querySelectAll('.answer-btn');
     buttons.forEach(function(button) {
     button.addEventListener('click', function() {
-let answer = this.getAttribute('data-answer');
+    let output = document.getElementById('output');
+    let answer = this.getAttribute('data-answer');
     output.textContent = "You selected: " + answer;
  })
 });
