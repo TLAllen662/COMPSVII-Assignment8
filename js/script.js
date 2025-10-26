1ba7270f7996a21d132fe933561b3992aad3ddb5
TLAllen662 <tlallen1419@gmail.com>
2025-10-26 16:25:56 -0500

    Refactor output messages for answer buttons to reflect choices A, B, C, and D

--- js/script.js
+++ js/script.js
@@ -3,18 +3,20 @@ let buttons = document.querySelectAll('.answer-btn');
     buttons.forEach(function(button) {
     button.addEventListener('click', function() {
     let mood = button.getAttribute('data-mood');
+    let buttonID = button.dataset.buttonid
+    let response = button.dataset.mood;
     let output = document.getElementById('output');
     let answer = this.getAttribute('data-answer');
     output.textContent = 'You selected:' + answer;
 
     if (mood === '1') {
-        output.textContent = 'Football Fan!';
+        output.textContent = 'You chose A';
     } else if (mood === '2') {
-        output.textContent = 'Soccer Fan!';
+        output.textContent = 'You chose B';
     } else if (mood === '3') {
-        output.textContent = 'Basketball Fan!';
+        output.textContent = 'You chose C';
     } else if (mood === '4') {
-        output.textContent = 'Baseball Fan!';
+        output.textContent = 'You chose D';
     }
     
 })
